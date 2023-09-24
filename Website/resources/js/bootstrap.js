db7a055b5c93c1fdc9d6ebee3593138d5f2b7bc5
xxklapauciusxx <brendanroggema@Outlook.com>
2023-09-24 02:44:18 +0200

    added breeze

--- Website/resources/js/bootstrap.js
+++ Website/resources/js/bootstrap.js
@@ -3,9 +3,10 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
 import axios from 'axios';
+require('bootstrap');
 window.axios = axios;
+
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
